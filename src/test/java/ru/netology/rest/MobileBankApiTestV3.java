754885b15243fffb1f9dc87f74b0ceb8b61c1a86
Oleg <maczak94@yandex.ru>
2020-07-04 17:21:25 +0700

    update files TEST

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -20,6 +21,7 @@ class MobileBankApiTestV3 {
       // Проверки
       .then()
           .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
           // специализированные проверки - лучше
           .contentType(ContentType.JSON)
           .body("", hasSize(3))
--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -5,7 +5,7 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
-
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV6 {
@@ -29,6 +29,7 @@ class MobileBankApiTestV6 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
